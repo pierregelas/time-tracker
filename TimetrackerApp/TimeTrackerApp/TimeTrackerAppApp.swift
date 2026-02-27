dc56f1c7fa7b1dfe8053688cee3c63701d45ead6
Pierre Gelas <pierregelas007@gmail.com>
2026-02-27 12:50:22 +0100

    Add clean Xcode macOS SwiftUI project structure

---
+++ TimetrackerApp/TimeTrackerApp/TimeTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerAppApp.swift
+//  TimeTrackerApp
+//
+//  Created by Pierre on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimetrackerApp/TimeTrackerAppTests/TimeTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerAppTests.swift
+//  TimeTrackerAppTests
+//
+//  Created by Pierre on 2026-02-27.
+//
+
+import Testing
+@testable import TimeTrackerApp
+
+struct TimeTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
